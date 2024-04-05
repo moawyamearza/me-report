a024ae4edef992fc2af6e991966857fcc1f9fca5
Moawya Mearza <moawyamearza@gmail.com>
2024-04-05 16:38:28 +0200

    fix valdeing för klass Game service

--- src/Card/GameService.php
+++ src/Card/GameService.php
@@ -6,7 +6,15 @@ use App\Card\CardGraphic as CardG;
 
 class GameService
 {
-    
+    /**
+     * Draw cards from the deck.
+     * 
+     * @param array $cards The current cards in hand.
+     * @param array $new The new cards drawn.
+     * @param array $arrcardvalue Array containing values of the cards.
+     * @param int $valueSum The sum of card values.
+     * @return array The updated state after drawing cards.
+     */
     public function draw($cards, $new, $arrcardvalue, $valueSum)
     {
         if (!empty($cards) && $valueSum <= 21) {
